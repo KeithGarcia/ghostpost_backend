134a906008495f172c5c21daa5015c536c4dc49f
Keith Garcia <Keith.Garcia444@gmail.com>
2020-10-02 19:15:43 -0400

    done

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    is_boast = models.BooleanField()
+    post_text = models.CharField(max_length=200)
+    up_votes = models.IntegerField(default=0)
+    down_votes = models.IntegerField(default=0)
+    submission_date = models.DateTimeField(default=timezone.now)
+    score = models.IntegerField(default=0)
--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
 
-# Create your views here.
+from homepage.models import Post
+from homepage.serializers import PostSerializer
+
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows posts to be viewed or edited.
+    """
+    queryset = Post.objects.all().order_by('-submission_date')
+    serializer_class = PostSerializer
+
+    @action(detail=False)
+    def boasts(self, request):
+        boasts = Post.objects.filter(
+            is_boast=True).order_by('-submission_date')
+        serializer = self.get_serializer(boasts, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False)
+    def roasts(self, request):
+        roasts = Post.objects.filter(
+            is_boast=False).order_by('-submission_date')
+        serializer = self.get_serializer(roasts, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['post'])
+    def add_upvote(self, request, pk=None):
+        post = self.get_object()
+        post.up_votes += 1
+        post.score = post.up_votes - post.down_votes
+        post.save()
+        return Response({'status': 'post upvoted'})
+
+    @action(detail=True, methods=['post'])
+    def add_downvote(self, request, pk=None):
+        post = self.get_object()
+        post.down_votes += 1
+        post.score = post.up_votes - post.down_votes
+        post.save()
+        return Response({'status': 'post downvoted'})
