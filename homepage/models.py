d834a5bb20ab7300ccd0225cf664570e0f99af64
KeithGarcia <61164323+KeithGarcia@users.noreply.github.com>
2020-10-04 17:33:18 -0400

    changes in thought

--- homepage/models.py
+++ homepage/models.py
@@ -3,9 +3,13 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    is_boast = models.BooleanField()
     post_text = models.CharField(max_length=200)
     up_votes = models.IntegerField(default=0)
     down_votes = models.IntegerField(default=0)
     submission_date = models.DateTimeField(default=timezone.now)
-    score = models.IntegerField(default=0)
+    choices = ((True, 'Boast'), (False, 'Roast'))
+    roast_or_boast = models.BooleanField(choices=choices, default=True)
+        
+    @property
+    def votes(self):
+        return self.up_votes + self.down_votes
